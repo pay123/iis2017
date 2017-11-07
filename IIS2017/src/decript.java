89eb7bba8f91e942c46f9b62416785bbee99869e
pay123 <fpayerl@gmail.com>
2017-11-07 16:07:53 +0100

    -

--- IIS2017/src/decript.java
+++ IIS2017/src/decript.java
@@ -3,19 +3,39 @@ import java.math.BigInteger;
 
 public class decript {
 	
-	BigInteger buildint (String c){
+	static BigInteger buildint (String cipher){
 		
-		char[] c_char = c.toCharArray();
+		char[] c_char = cipher.toCharArray();
+		BigInteger c_int = BigInteger.ZERO; 
+		
+		int[] array;
+		
+	    for(char c : c_char)
+	    {
+	        int temp = (int)c;
+	        int temp_integer = 65; //for upper case
+	if(temp<=90 & temp>=65)
+	    System.out.print(temp-temp_integer);
 		
 		
+	
+		c_int.add(BigInteger.valueOf(temp-temp_integer));
 		
-		BigInteger c_int = new BigInteger("0");
+	    }
+		
+		
 		return c_int;
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		BigInteger c  = buildint("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+		//buildint("ZZZZZZZZZZZZZZZAAAAAAAAAAAA");
+		System.out.println("");
+		System.out.println(c);
+		
 	}
 
 }

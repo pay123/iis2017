4964369a69bda141964ce141142dafdcad527cfc
pay123 <fpayerl@gmail.com>
2017-11-07 15:20:28 +0100

    initial

---
+++ IIS2017/src/decript.java
@@ -0,0 +1,21 @@
+import java.math.BigInteger;
+
+
+public class decript {
+	
+	BigInteger buildint (String c){
+		
+		char[] c_char = c.toCharArray();
+		
+		
+		
+		BigInteger c_int = new BigInteger("0");
+		return c_int;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

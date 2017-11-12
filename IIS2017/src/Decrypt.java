078af9de1a7f2a24570add77762320c0288769cb
Lukas Ber <l.ber@student.tugraz.at>
2017-11-12 21:08:12 +0100

    Updated encoding

--- IIS2017/src/Decrypt.java
+++ IIS2017/src/Decrypt.java
@@ -1,45 +1,92 @@
+import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.util.Random;
 
+import static java.lang.Math.pow;
 
-public class Decrypt {
+// pubkey (e, n) = (65537, 581696833723949335177)
+// private key d?
+
+// https://stackoverflow.com/questions/16310871/in-rsa-encryption-how-do-i-find-d-given-p-q-e-and-c
+// d = e.modInverse(p_1.multiply(q_1))
+
+public class Decrypt
+{
+    public static final BigInteger E = BigInteger.valueOf(65537);
+    public static final BigInteger N = new BigInteger("581696833723949335177");
+
+	private static void buildInt(String s)
+    {
+        char[] cipher = s.toCharArray();
+        StringBuilder m = new StringBuilder();
+        StringBuilder c = new StringBuilder();
+
+        int i = 0;
+        for (char u : cipher)
+        {
+            if (u >= 'A' && u <= 'Z')
+            {
+                c.append(u - 'A');
+                m.append(new BigDecimal((u - 'A') * pow(26, i)).toBigInteger());
+            }
+            i++;
+        }
+
+        System.out.println("c: " + c);
+        System.out.println("m: " + m);
+    }
 	
-	static BigInteger buildint (String cipher){
-		
-		char[] c_char = cipher.toCharArray();
-		//BigInteger c_int = BigInteger.ZERO; 
-		
-		String number = "";
-		
-		
-	    for(char c : c_char)
-	    {
-	        int temp = (int)c;
-	        int temp_integer = 65; 
-	        if(temp<=90 & temp>=65){
-	        //	System.out.print(temp-temp_integer);
-	        	int num = temp-temp_integer;
-	        	number = number + num;
-	   
-	        }
-		
-	    }
-	  //  System.out.println("");
-	  //  System.out.println(number);
-	    BigInteger c_int = new BigInteger(number); 
-		
-		return c_int;
-	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args)
+    {
+        buildInt("GVHIUXNYQXWUGB");
+//        computeRSAFactors(E, N);
+    }
 
-		
-		BigInteger c  = buildint("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-		BigInteger c1 = buildint("HALLOHALLOHALLO");
-		
-		System.out.println(c);
-		System.out.println(c1);
-		
-	}
+    private static void computeRSAFactors(BigInteger e, BigInteger n)
+    {
+//        n = BigInteger.valueOf(10142789312725007L);
+//        e = BigInteger.valueOf(8114231289041741L);
+        final BigInteger d = BigInteger.valueOf(5);
+
+        final long t0 = System.currentTimeMillis();
+
+        final BigInteger kTheta = d.multiply(e).subtract(BigInteger.ONE);
+        final int exponentOfTwo = kTheta.getLowestSetBit();
+
+        final Random random = new Random();
+        BigInteger factor = BigInteger.ONE;
+        do
+        {
+            final BigInteger a = nextA(n, random);
+
+            for (int i = 1; i <= exponentOfTwo; i++)
+            {
+                final BigInteger exponent = kTheta.shiftRight(i);
+                final BigInteger power = a.modPow(exponent, n);
+
+                final BigInteger gcd = n.gcd(power.subtract(BigInteger.ONE));
+                if (!factor.equals(BigInteger.ONE))
+                {
+                    break;
+                }
+            }
+        }
+        while (factor.equals(BigInteger.ONE));
+
+        final long t1 = System.currentTimeMillis();
+
+        System.out.printf("%s %s (%dms)\n", factor, n.divide(factor), t1 - t0);
+    }
+
+    private static BigInteger nextA(final BigInteger n, final Random random)
+    {
+        BigInteger r;
+        do
+        {
+            r = new BigInteger(n.bitLength(), random);
+        }
+        while (r.signum() == 0 || r.compareTo(n) >= 0);
+        return r;
+    }
 
 }
